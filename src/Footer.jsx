3968f3aeee976a57e9bf470e93a8390037472e8a
Marco <marcopatzelt7@gmail.com>
2024-09-01 20:30:43 +0200

    Add keyword_list.py and update project structure

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-black text-white py-6">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-        <p>&copy; 2024 MyApp. All Rights Reserved. Made by Marco .P</p>
+        <p>&copy; 2024. All Rights Reserved. Made by Marco .P</p>
       </div>
     </footer>
   );
