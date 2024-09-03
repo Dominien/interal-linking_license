4210935b05ea6dadf32cfda6ac8d1e498d2f8fe1
Marco <marcopatzelt7@gmail.com>
2024-09-03 08:32:44 +0200

    Add keyword_list.py and update project structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,25 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
-import InternalLinkingTool from './components/InternalLinkingTool'; // Import your InternalLinkingTool component
+import InternalLinkingTool from './components/InternalLinkingTool';
+import Profile from './components/Profile';
+import Navbar from './Navbar'; // Import Navbar component from src
+import Footer from './Footer'; // Import Footer component from src
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Login onLogin={() => console.log('Logged in!')} />} />
-        <Route path="/internal-linking-tool" element={<InternalLinkingTool />} />
-      </Routes>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Login onLogin={() => console.log('Logged in!')} />} />
+          <Route path="/internal-linking-tool" element={<InternalLinkingTool />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   );
-  
 }
 
 export default App;
