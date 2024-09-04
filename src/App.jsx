4f2b3823515db9e6bc24b8d36efdd38a8a255aaf
Marco <marcopatzelt7@gmail.com>
2024-09-04 16:22:21 +0200

    Add keyword_list.py and update project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Login from './components/Login';
 import InternalLinkingTool from './components/InternalLinkingTool';
 import Profile from './components/Profile';
 import Navbar from './Navbar'; // Import Navbar component from src
 import Footer from './Footer'; // Import Footer component from src
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Login onLogin={() => console.log('Logged in!')} />} />
-          <Route path="/internal-linking-tool" element={<InternalLinkingTool />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
-  );
-}
+const App = () => {
+  const location = useLocation(); // Get the current route location
 
-export default App;
+  return (
+    <div>
+      {/* Conditionally render Navbar only if the route is not "/" */}
+      {location.pathname !== '/' && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Login onLogin={() => console.log('Logged in!')} />} />
+        <Route path="/internal-linking-tool" element={<InternalLinkingTool />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
+
+const AppWrapper = () => (
+  <Router>
+    <App />
+  </Router>
+);
+
+export default AppWrapper;
